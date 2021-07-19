11a6e688ae29ffff5a6199768f47f1af3d55be49
yjlee1012 <elj308@naver.com>
2021-07-19 15:34:20 +0900

    getiing the Nweets(no realtime & realtime)

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,16 @@ import {authService} from 'fbase';
 function App() {
   const [init,setInit] = useState(false);
   const [isLoggedIn,setIsLoggedIn] = useState(false);
+  const [userObj,setUserObj] = useState(null);
   // authService.currentUser는 로그인되었는지 알아차리지 못함
   // firebase를 기다려주어 로그인 되었다는 사실 알아차릴 수 있음
   useEffect(()=>{
     authService.onAuthStateChanged((user)=> {
       if(user){
-        setIsLoggedIn(true)
+        setIsLoggedIn(true);
+        setUserObj(user);
       }else{
-        setIsLoggedIn(false)
+        setIsLoggedIn(false);
       }
       //init이 false라면 router를 숨길 것이기 때문
       setInit(true)
@@ -22,7 +24,7 @@ function App() {
 
   return (
     <>
-    {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing..."}
+    {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/> : "Initializing..."}
     <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
     </>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,44 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import { dbService } from 'fbase';
 
-const Home = () =>{
+const Home = ({userObj}) =>{
     const [nweet,setNweet] = useState("");
+    const [nweets,setNweets] = useState([]);
+    
+    //getting the nweets
+    // //이 방식은 오래된 데이터를 가져온다.(no real time) 새로 생성/변경된 데이터는 새로고침해야 반영된다.
+    // const getNweets = async () => {
+    //     const dbNweets = await dbService.collection("nweets").get();
+    //     dbNweets.forEach((document)=>{
+    //         const nweetObject = {
+    //             ...document.data(),
+    //             id : document.id,
+    //         }
+    //         setNweets(prev => [nweetObject,...prev])
+    //     });
+    // }
+
+    useEffect(()=>{
+        // getNweets();
+        
+        //getting nweets real-time (onSnapshot 이용)
+        dbService.collection('nweets').orderBy("createdAt","desc").onSnapshot((snapshot)=>{
+            const nweetArray = snapshot.docs.map((doc)=>({
+                id:doc.id,
+                ...doc.data(),
+            }))
+            setNweets(nweetArray);
+        })
+    },[])
+
     const onSubmit = async (e) =>{
         e.preventDefault();
         await dbService.collection("nweets").add({
             //can write whatever data you want
-            nweet,
+            text:nweet,
             createdAt:Date.now(),
+            creatorId:userObj.uid,
+            // creatorEmail:userObj.email,
         })
         setNweet('');
     }
@@ -16,12 +46,20 @@ const Home = () =>{
         const {target : {value} }= e;
         setNweet(value);
     }
+    console.log(nweets)
     return(
         <div>
             <form onSubmit={onSubmit}>
                 <input onChange={onChange} value={nweet} type="text" placeholder="What's on your mind?" maxLength={120}></input>
                 <input type="submit" value="Nweet"/>
             </form>
+            <div>
+                {nweets.map(nweet=>(
+                    <div key={nweet.id}>
+                        <h4>{nweet.text}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
